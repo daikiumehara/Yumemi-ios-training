4803838bd20182ba7664930b7a7c2b7cdcebe65c
梅原 奈輝 <d_umehara@MacBook-Pro.local>
2022-05-06 14:01:23 +0900

    Clientの作成 Weatherの列挙型作成

---
+++ Yumemi-ios-training/Enums/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  Yumemi-ios-training
+//
+//  Created by 梅原 奈輝 on 2022/05/06.
+//
+
+import Foundation
+
+enum Weather: String {
+    case sunny
+    case cloudy
+    case rainy
+}
