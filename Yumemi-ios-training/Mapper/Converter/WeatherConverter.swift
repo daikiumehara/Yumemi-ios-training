f0f3dd86cb5f9fbf185792e836c57fc6900fec79
daikiumehara <daikiumehara3@gmail.com>
2022-05-09 15:22:53 +0900

    機能を完成させました

--- Yumemi-ios-training/Mapper/Converter/WeatherConverter.swift
+++ Yumemi-ios-training/Mapper/Converter/WeatherConverter.swift
@@ -2,23 +2,15 @@
 //  WeatherConverter.swift
 //  Yumemi-ios-training
 //
-//  Created by 梅原 奈輝 on 2022/05/06.
+//  Created by 梅原 奈輝 on 2022/05/09.
 //
 
-import UIKit
+import Foundation
 
 struct WeatherConverter {
-    static func convert(weather: Weather) -> WeatherImageData {
-        switch weather {
-        case.sunny:
-            return WeatherImageData(image: #imageLiteral(resourceName: "sunny").withTintColor(.red))
-        case .cloudy:
-            return WeatherImageData(image: #imageLiteral(resourceName: "cloudy").withTintColor(.gray))
-        case .rainy:
-            return WeatherImageData(image: #imageLiteral(resourceName: "rainy").withTintColor(.blue))
-        case .none:
-            // 画像がないので一旦sunnyのgrayを返す
-            return WeatherImageData(image: #imageLiteral(resourceName: "sunny").withTintColor(.gray))
-        }
+    static func convert(data: WeatherInfo) -> WeatherData {
+        return WeatherData(imageData: WeatherImageConverter.convert(weather: data.weather),
+                           maxTemp: data.maxTemp.description,
+                           minTemp: data.minTemp.description)
     }
 }
--- Yumemi-ios-training/Mapper/Converter/WeatherImageConverter.swift
+++ Yumemi-ios-training/Mapper/Converter/WeatherImageConverter.swift
@@ -9,16 +9,16 @@ import UIKit
 
 struct WeatherImageConverter {
     static func convert(weather: Weather) -> UIImage {
-            switch weather {
-            case.sunny:
-                return #imageLiteral(resourceName: "sunny").withTintColor(.red)
-            case .cloudy:
-                return #imageLiteral(resourceName: "cloudy").withTintColor(.gray)
-            case .rainy:
-                return #imageLiteral(resourceName: "rainy").withTintColor(.blue)
-            case .none:
-                // 画像がないので一旦sunnyのgrayを返す
-                return #imageLiteral(resourceName: "sunny").withTintColor(.gray)
-            }
+        switch weather {
+        case.sunny:
+            return #imageLiteral(resourceName: "sunny").withTintColor(.red)
+        case .cloudy:
+            return #imageLiteral(resourceName: "cloudy").withTintColor(.gray)
+        case .rainy:
+            return #imageLiteral(resourceName: "rainy").withTintColor(.blue)
+        case .none:
+            // 画像がないので一旦sunnyのgrayを返す
+            return #imageLiteral(resourceName: "sunny").withTintColor(.gray)
         }
+    }
 }
