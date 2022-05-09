752ff5dda4f35cc354a14adf2e6dae97e77f8d35
daikiumehara <daikiumehara3@gmail.com>
2022-05-09 15:22:53 +0900

    機能を完成させました

--- Yumemi-ios-training/Mapper/Converter/WeatherImageConverter.swift
+++ Yumemi-ios-training/Mapper/Converter/WeatherImageConverter.swift
@@ -9,16 +9,16 @@ import UIKit
 
 struct WeatherImageConverter {
     static func convert(weather: Weather) -> WeatherImageData {
-            switch weather {
-            case.sunny:
-                return WeatherImageData(image: #imageLiteral(resourceName: "sunny").withTintColor(.red))
-            case .cloudy:
-                return WeatherImageData(image: #imageLiteral(resourceName: "cloudy").withTintColor(.gray))
-            case .rainy:
-                return WeatherImageData(image: #imageLiteral(resourceName: "rainy").withTintColor(.blue))
-            case .none:
-                // 画像がないので一旦sunnyのgrayを返す
-                return WeatherImageData(image: #imageLiteral(resourceName: "sunny").withTintColor(.gray))
-            }
+        switch weather {
+        case.sunny:
+            return WeatherImageData(image: #imageLiteral(resourceName: "sunny").withTintColor(.red))
+        case .cloudy:
+            return WeatherImageData(image: #imageLiteral(resourceName: "cloudy").withTintColor(.gray))
+        case .rainy:
+            return WeatherImageData(image: #imageLiteral(resourceName: "rainy").withTintColor(.blue))
+        case .none:
+            // 画像がないので一旦sunnyのgrayを返す
+            return WeatherImageData(image: #imageLiteral(resourceName: "sunny").withTintColor(.gray))
         }
+    }
 }
