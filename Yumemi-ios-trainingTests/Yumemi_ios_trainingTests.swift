081874a36f5cdd18f714401600650af982cf9d99
daikiumehara <daikiumehara3@gmail.com>
2022-05-20 15:44:22 +0900

    rebaseによって変更されたプロパティに合わせてテストを修正しました

--- Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
+++ Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
@@ -19,46 +19,38 @@ class Yumemi_ios_trainingTests: XCTestCase {
         vc.inject(presenter: presenter)
         presenter.view = vc
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-   
-    }
     
     func test_天気予報がsunnyだったら_画面に晴れ画像が表示されること() {
         let data = WeatherInfo(maxTemp: 10, minTemp: 5, weather: .sunny)
         presenter.changeWeather(weather: data)
         let weatherData = WeatherConverter.convert(data: data)
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.createImage())
+        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.image)
         
         presenter.data = data
         vc.onTapReloadButton("")
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.createImage())
+        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.image)
     }
     
     func test_天気予報がcloudyだったら_画面に曇り画像が表示されること() {
         let data = WeatherInfo(maxTemp: 10, minTemp: 5, weather: .cloudy)
         presenter.changeWeather(weather: data)
         let weatherData = WeatherConverter.convert(data: data)
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.createImage())
+        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.image)
         
         presenter.data = data
         vc.onTapReloadButton("")
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.createImage())
+        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.image)
     }
     
     func test_天気予報がrainyだったら_画面に雨画像が表示されること() {
         let data = WeatherInfo(maxTemp: 10, minTemp: 5, weather: .rainy)
         presenter.changeWeather(weather: data)
         let weatherData = WeatherConverter.convert(data: data)
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.createImage())
+        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.image)
         
         presenter.data = data
         vc.onTapReloadButton("")
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.createImage())
+        XCTAssertEqual(vc.weatherImageView.image!, weatherData.imageData.image)
     }
     
     func test_天気予報の最高気温がUILabelに反映されること() {
@@ -82,12 +74,4 @@ class Yumemi_ios_trainingTests: XCTestCase {
         vc.onTapReloadButton("")
         XCTAssertEqual(vc.minTempLabel.text!, weatherData.minTemp)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
