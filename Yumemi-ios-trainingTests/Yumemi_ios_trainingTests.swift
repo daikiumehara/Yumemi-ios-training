72e523687e8785a145d3de4269452179f2d3f0c8
daikiumehara <daikiumehara3@gmail.com>
2022-05-25 18:23:20 +0900

    非同期処理に合わせてテストを修正しました

--- Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
+++ Yumemi-ios-trainingTests/Yumemi_ios_trainingTests.swift
@@ -11,7 +11,7 @@ import XCTest
 class Yumemi_ios_trainingTests: XCTestCase {
     var vc: ViewController!
     var presenter: MockMainPresenter!
-
+    
     override func setUpWithError() throws {
         vc = ViewController.instantiate()
         vc.loadViewIfNeeded()
@@ -22,56 +22,67 @@ class Yumemi_ios_trainingTests: XCTestCase {
     
     func test_天気予報がsunnyだったら_画面に晴れ画像が表示されること() {
         let data = WeatherInfo(maxTemp: 10, minTemp: 5, weather: .sunny)
-        presenter.changeWeather(weather: data)
         let weatherData = WeatherUIDataConverter.convert(weatherInfo: data)
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.image)
         
         presenter.data = data
-        vc.onTapReloadButton("")
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.image)
+        waitFetchData(funcName: #function, timeout: 5.0) {
+            self.vc.onTapReloadButton("")
+        }
+        XCTAssertEqual(self.vc.weatherImageView.image!, weatherData.image)
     }
     
     func test_天気予報がcloudyだったら_画面に曇り画像が表示されること() {
         let data = WeatherInfo(maxTemp: 10, minTemp: 5, weather: .cloudy)
-        presenter.changeWeather(weather: data)
         let weatherData = WeatherUIDataConverter.convert(weatherInfo: data)
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.image)
         
         presenter.data = data
-        vc.onTapReloadButton("")
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.image)
+        waitFetchData(funcName: #function, timeout: 5.0) {
+            self.vc.onTapReloadButton("")
+        }
+        XCTAssertEqual(self.vc.weatherImageView.image!, weatherData.image)
+        
     }
     
     func test_天気予報がrainyだったら_画面に雨画像が表示されること() {
         let data = WeatherInfo(maxTemp: 10, minTemp: 5, weather: .rainy)
-        presenter.changeWeather(weather: data)
         let weatherData = WeatherUIDataConverter.convert(weatherInfo: data)
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.image)
         
         presenter.data = data
-        vc.onTapReloadButton("")
-        XCTAssertEqual(vc.weatherImageView.image!, weatherData.image)
+        waitFetchData(funcName: #function, timeout: 5.0) {
+            self.vc.onTapReloadButton("")
+        }
+        XCTAssertEqual(self.vc.weatherImageView.image!, weatherData.image)
+        
     }
     
     func test_天気予報の最高気温がUILabelに反映されること() {
         let data = WeatherInfo(maxTemp: 10, minTemp: 5, weather: .sunny)
-        presenter.changeWeather(weather: data)
         let weatherData = WeatherUIDataConverter.convert(weatherInfo: data)
-        XCTAssertEqual(vc.maxTempLabel.text!, weatherData.maxTemp)
         
         presenter.data = data
-        vc.onTapReloadButton("")
-        XCTAssertEqual(vc.maxTempLabel.text!, weatherData.maxTemp)
+        waitFetchData(funcName: #function, timeout: 5.0) {
+            self.vc.onTapReloadButton("")
+        }
+        XCTAssertEqual(self.vc.maxTempLabel.text!, weatherData.maxTemp)
     }
     
     func test_天気予報の最低気温がUILabelに反映されること() {
         let data = WeatherInfo(maxTemp: 10, minTemp: 5, weather: .sunny)
-        presenter.changeWeather(weather: data)
         let weatherData = WeatherUIDataConverter.convert(weatherInfo: data)
-        XCTAssertEqual(vc.minTempLabel.text!, weatherData.minTemp)
         
         presenter.data = data
-        vc.onTapReloadButton("")
-        XCTAssertEqual(vc.minTempLabel.text!, weatherData.minTemp)
+        waitFetchData(funcName: #function, timeout: 5.0) {
+            self.vc.onTapReloadButton("")
+        }
+        XCTAssertEqual(self.vc.minTempLabel.text!, weatherData.minTemp)
+    }
+    
+    private func waitFetchData(funcName: String, timeout: TimeInterval, handler: @escaping () -> Void) {
+        let exp = XCTestExpectation(description: "\(funcName)の待機がタイムアウトしました")
+        DispatchQueue.global().async {
+            handler()
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: timeout)
     }
 }
