0409b8e1ec444eecaa75c214abda6620990e0078
daikiumehara <daikiumehara3@gmail.com>
2022-06-03 13:51:32 +0900

    メソッドをprivateにしました

--- Yumemi-ios-training/DIContainer/DIContainer.swift
+++ Yumemi-ios-training/DIContainer/DIContainer.swift
@@ -20,12 +20,12 @@ struct DIContainer {
     }
     
     //MARK: - UseCase
-    static func makeWeatherUseCase(weatherRepository: WeatherRepositoryProtocol) -> WeatherUseCaseProtocol {
+    static private func makeWeatherUseCase(weatherRepository: WeatherRepositoryProtocol) -> WeatherUseCaseProtocol {
         return WeatherUseCase(weatherRepository: weatherRepository)
     }
     
     //MARK: - Repository
-    static func makeWeatherRepository(weatherClient: WeatherClientProtocol) -> WeatherRepositoryProtocol {
+    static private func makeWeatherRepository(weatherClient: WeatherClientProtocol) -> WeatherRepositoryProtocol {
         return WeatherRepository(weatherClient: weatherClient)
     }
 }
