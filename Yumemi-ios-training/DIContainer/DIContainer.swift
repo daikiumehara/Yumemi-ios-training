54f59f76400ca0380ad245ae93ba8d7497c66c3a
daikiumehara <daikiumehara3@gmail.com>
2022-06-03 15:02:00 +0900

    不要な差分を削除しました

--- Yumemi-ios-training/DIContainer/DIContainer.swift
+++ Yumemi-ios-training/DIContainer/DIContainer.swift
@@ -20,12 +20,12 @@ struct DIContainer {
     }
     
     //MARK: - UseCase
-    static private func makeWeatherUseCase(weatherRepository: WeatherRepositoryProtocol) -> WeatherUseCaseProtocol {
+    private static func makeWeatherUseCase(weatherRepository: WeatherRepositoryProtocol) -> WeatherUseCaseProtocol {
         return WeatherUseCase(weatherRepository: weatherRepository)
     }
     
     //MARK: - Repository
-    static private func makeWeatherRepository(weatherClient: WeatherClientProtocol) -> WeatherRepositoryProtocol {
+    private static func makeWeatherRepository(weatherClient: WeatherClientProtocol) -> WeatherRepositoryProtocol {
         return WeatherRepository(weatherClient: weatherClient)
     }
 }
