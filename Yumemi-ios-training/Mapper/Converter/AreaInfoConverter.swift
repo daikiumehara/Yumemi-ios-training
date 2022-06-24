6ffb167372b013b0a6b30c3867490d057c1f1917
daikiumehara <daikiumehara3@gmail.com>
2022-06-24 15:01:55 +0900

    AreaInfoを作成しました

---
+++ Yumemi-ios-training/Mapper/Converter/AreaInfoConverter.swift
@@ -0,0 +1,8 @@
+//
+//  AreaInfoConverter.swift
+//  Yumemi-ios-training
+//
+//  Created by 梅原 奈輝 on 2022/06/24.
+//
+
+import Foundation
---
+++ Yumemi-ios-training/Model/AreaInfo.swift
@@ -0,0 +1,12 @@
+//
+//  AreaInfo.swift
+//  Yumemi-ios-training
+//
+//  Created by 梅原 奈輝 on 2022/05/16.
+
+import Foundation
+
+struct AreaInfo {
+    let weatherInfo: WeatherInfo
+    let area: String
+}
