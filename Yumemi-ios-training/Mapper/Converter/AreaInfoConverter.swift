cbd87ec73dfe3e6fb4d9f81c110efeaaca16085b
daikiumehara <daikiumehara3@gmail.com>
2022-06-24 15:02:07 +0900

    AreaInfoConverterを作成しました

--- Yumemi-ios-training/Mapper/Converter/AreaInfoConverter.swift
+++ Yumemi-ios-training/Mapper/Converter/AreaInfoConverter.swift
@@ -2,7 +2,17 @@
 //  AreaInfoConverter.swift
 //  Yumemi-ios-training
 //
-//  Created by 梅原 奈輝 on 2022/06/24.
+//  Created by 梅原 奈輝 on 2022/05/16.
 //
-
 import Foundation
+
+struct AreaInfoConverter {
+    static func convert(infraAreaInfo: InfraAreaInfo) -> AreaInfo {
+        AreaInfo(weatherInfo: WeatherInfoConverter.convert(infraWeatherInfo: infraAreaInfo.weatherInfo),
+                 area: infraAreaInfo.area)
+    }
+
+    static func convert(infraAreaInfoList: [InfraAreaInfo]) -> [AreaInfo] {
+        infraAreaInfoList.map { convert(infraAreaInfo: $0) }
+    }
+}
