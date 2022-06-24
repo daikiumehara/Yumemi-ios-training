12b2de079750e7353b9a25f334c9bbd929087c65
daikiumehara <daikiumehara3@gmail.com>
2022-06-24 15:04:05 +0900

    AreaDataを作成しました

---
+++ Yumemi-ios-training/UIModel/AreaData.swift
@@ -0,0 +1,13 @@
+//
+//  AreaData.swift
+//  Yumemi-ios-training
+//
+//  Created by 梅原 奈輝 on 2022/05/16.
+//
+
+import Foundation
+
+struct AreaData {
+    let weatherUIData: WeatherUIData
+    let area: String
+}
