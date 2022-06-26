a8541f74e3636556ad8a22cd39b2e13ceb90c388
daikiumehara <daikiumehara3@gmail.com>
2022-06-27 02:59:11 +0900

    不要な変更を削除しました

--- Yumemi-ios-trainingUITests/Yumemi_ios_trainingUITests.swift
+++ Yumemi-ios-trainingUITests/Yumemi_ios_trainingUITests.swift
@@ -8,20 +8,20 @@
 import XCTest
 
 class Yumemi_ios_trainingUITests: XCTestCase {
-    
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
+
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        
+
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-    
+
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
+
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
@@ -43,7 +43,7 @@ class Yumemi_ios_trainingUITests: XCTestCase {
         let minTempLabel = app.staticTexts["MinTempLabel"]
         XCTAssertEqual(minTempLabel.label, "10")
     }
-    
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
