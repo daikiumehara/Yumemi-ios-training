7b1188374c67e127a6fd6202cb1adef76722ada0
daikiumehara <daikiumehara3@gmail.com>
2022-07-01 12:17:59 +0900

    不要なファイルを削除し、それに合わせてコードを修正しました

--- Yumemi-ios-training/DIContainer/DIContainer.swift
+++ Yumemi-ios-training/DIContainer/DIContainer.swift
@@ -8,25 +8,8 @@
 import Foundation
 
 struct DIContainer {
-    //MARK: - Presenter
-    static func getWeatherPresenter(view: WeatherViewProtocol, weatherClient: WeatherClientProtocol) -> WeatherPresenterProtocol {
-        let useCase = self.makeWeatherUseCase(
-            weatherRepository: self.makeWeatherRepository(weatherClient: weatherClient)
-        )
-        let presenter = WeatherPresenter(view: view,
-                                      weatherUseCase: useCase)
-        useCase.output = presenter
-        return presenter
-    }
-    
-    //MARK: - UseCase
-    private static func makeWeatherUseCase(weatherRepository: WeatherRepositoryProtocol) -> WeatherUseCaseProtocol {
-        WeatherUseCase(weatherRepository: weatherRepository,
-                       callbackQueue: .main)
-    }
-    
     //MARK: - Repository
-    private static func makeWeatherRepository(weatherClient: WeatherClientProtocol) -> WeatherRepositoryProtocol {
-        WeatherRepository(weatherClient: weatherClient)
+    static func makeWeatherRepository() -> WeatherRepositoryProtocol {
+        WeatherRepository(weatherClient: WeatherClient())
     }
 }
